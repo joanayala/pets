7365069bbe93b594a371009f12cc72bcab5877ae
Joan C <jflaco85@gmail.com>
2024-04-29 18:04:46 -0500

    Signin

---
+++ src/backend/signin.php
@@ -0,0 +1,27 @@
+<?php
+    include('../../config/database.php');
+
+    $email = $_POST['email'];
+    $passwd = $_POST['passwd'];
+    $enc_pass = md5($passwd);
+
+    $sql = "
+        SELECT
+            *
+        FROM
+            users
+        WHERE
+            email = '$email' AND
+            password = '$enc_pass'
+        LIMIT 1
+    ";
+
+    $result = pg_query($conn, $sql);
+    $total = pg_num_rows($result);
+
+    if ($total > 0){
+        echo "Login OK";
+    }else{
+        echo "Credenciales incorrectas";
+    }
+?>
